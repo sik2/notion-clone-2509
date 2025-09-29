6f4aed7a389fe5a8fad94b8dec34a9885ea0a208
sik2 <bickwang123@gmail.com>
2025-09-29 20:27:38 +0900

    4

--- components/SidebarPage.js
+++ components/SidebarPage.js
@@ -1,10 +1,52 @@
 export default function SidebarPage({ $target, initalState }) {
     this.state = initalState
     const $page = document.createElement('div')
+    $page.classList.add('listContainer')
+
+    this.createTreeView = (data) => {
+        let str = ''
+        for (const key in data) {
+            if (data[key].documents.length > 0) {
+                str += `
+                <li class="dataList">
+                    ${data[key].title}
+                    <button class="addBtn">➕</button>
+                    <button class="delBtn">🗑️</button>
+                    <ul>${this.createTreeView(data[key].documents)}</ul>
+                </li>`
+            } else {
+                str += `
+                <li class="dataList">
+                    ${data[key].title}
+                    <button class="addBtn">➕</button>
+                    <button class="delBtn">🗑️</button>
+                    <ul>${this.createTreeView(data[key].documents)}</ul>
+                </li>`
+            }
+        }
+
+        return str
+    }
+
     $page.innerHTML = `
         <ul>
             ${this.state
-                .map((row) => `<li>${row.id} / ${row.title}</li>`)
+                .map(
+                    (document) =>
+                        `<li class="dataList">
+                            ${document.title}
+                            <button class="addBtn">➕</button>
+                            <button class="delBtn">🗑️</button>
+                        </li>
+                        ${
+                            document.documents.length > 0
+                                ? `<ul>
+                                ${this.createTreeView(document.documents)}
+                            </ul>`
+                                : ''
+                        } 
+                    `
+                )
                 .join('')}
         </ul>
     `
